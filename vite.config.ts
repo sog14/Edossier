ba209fdac283ff32a6d3df72c69148ca791b4f8e
sog14 <stfsog14@gmail.com>
2026-03-07 21:49:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react()],
-    base: './',
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || process.env.GEMINI_API_KEY)
-    },
-    build: {
-      outDir: 'dist',
-      rollupOptions: {
-        input: {
-          main: './index.html'
-        }
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
       }
     }
-  };
+  }
 });
